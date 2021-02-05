6500fb2825f7a8d878c2a51578e9bee14afc68c8
LuukOudshoorn25 <luuk@lupanas.nl>
2021-02-05 12:21:53 +0100

    First figure working

--- Assignment1/plottinglib.py
+++ Assignment1/plottinglib.py
@@ -11,7 +11,7 @@ def plot_raw_data(df):
     plt.show()
 
 def plot_fig2_1(times, y, a, std, P, v, F):
-    fig, [[ax1,ax2],[ax3,ax4]] = plt.subplots(2,2, sharex=True,figsize=(6,6))
+    fig, [[ax1,ax2],[ax3,ax4]] = plt.subplots(2,2, sharex=True,figsize=(5,3.5))
     ax1.plot(times[1:], a[1:],color='black',lw=1)
     ax1.scatter(times, y, s=1,color='black')
     ax1.plot(times[1:], a[1:]+1.645*std,color='grey',lw=0.7)
@@ -28,4 +28,5 @@ def plot_fig2_1(times, y, a, std, P, v, F):
     ax4.set_ylabel('Prediction variance')
 
     plt.tight_layout()
+    plt.savefig('Fig21.pdf',bbox_inches='tight')
     plt.show()
