fixed smoothed state missing values, not yet the variance though.

Assignment1/kalman.py
@@ -153,20 +153,23 @@ class KFclass():
             F[t] = P[t]+sigma_eps2
             # K is defined as ratio of P and F
             Kt = P[t]/F[t] if np.isfinite(self.y[t]) else 0
-            v[t] = self.y[t]-a[t]
-            a[t+1] = a[t] + np.nan_to_num(Kt*v[t])
+            v[t] = np.nan_to_num(self.y[t]-a[t])
+            a[t+1] = a[t] + Kt*v[t]
             F[t] = P[t]+sigma_eps2
             P[t+1] = P[t]*(1-Kt)+sigma_eta2
         v[-1] = self.y[-1]-a[-1]
         F[-1] = P[-1]+sigma_eps2
         # Obtain smoothed state
         # Obtain all time values for L
-        L = self.pardict['sigma_eps2']/F
+        L = self.pardict['sigma_eps2'] / F
+        index_missing = np.argwhere(np.isnan(self.y))
+        L[index_missing] = 1
+
         # Do the recursion for r
         r = np.zeros(len(self.y))
         N = np.zeros(len(self.y))
         V = np.zeros(len(self.y))
-        
+
         for t in np.arange(len(self.y)-1,0,-1):
             r[t-1] = v[t]/F[t]+L[t]*r[t]
         for t in np.arange(len(self.y)-1,0,-1):