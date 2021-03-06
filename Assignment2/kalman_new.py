3d4359b38dda2b62afdfb9a901e65755fecaa991
LuukOudshoorn25 <luuk@lupanas.nl>
2021-03-06 15:43:04 +0100

    Updates WJ

---
+++ Assignment2/kalman_new.py
@@ -0,0 +1,119 @@
+from scipy.optimize import minimize, approx_fprime
+from plottinglib import *
+from scipy.optimize import minimize, approx_fprime
+
+from plottinglib import *
+
+
+class KFnew():
+    def __init__(self, df, Z, Q, H, T, R, d, c, var='dep_var'):
+        """Initialisation, where df is a pandas DataFrame and var is the name of the column to study and
+           init_pars is a dictionary with initial values"""
+        self.Z = Z
+        self.Q = Q
+        self.H = H
+        self.T = T
+        self.R = R
+        self.d = d
+        self.c = c
+
+        self.df = df
+        self.var = var
+        self.y = np.array(df[var].values.flatten())
+        self.times = df.index
+        self.options = {'eps': 1e-09,
+                        'maxiter': 2000}
+
+    def init_filter(self, a, P):
+        self.a_start = a
+        self.P_start = P
+
+    def __llik_fun__(self, par_ini):
+        # likelihood function of state space model
+        n = len(self.y)
+        _, __, P, v, F = self.iterate(plot=False, estimate=True, init_params=par_ini)
+        l = (n - 1) / 2 - 0.5 * np.sum(np.log(np.abs(F[1:]))) - 0.5 * np.sum((v[1:] ** 2) / F[1:])
+
+        llik = -np.sum(l)
+        return llik
+
+    def fit_model(self):
+        # Initialize at the initial values parsed to the class
+        phi = self.T
+        omega = self.c
+        sigma_eta2 = self.R
+
+        par_ini = [phi, omega, sigma_eta2]
+        # minimize the likelihood function
+        Lprime = lambda x: approx_fprime(x, self.__llik_fun__, 0.01)
+        est = minimize(self.__llik_fun__, x0=par_ini,
+                       options=self.options,
+                       method='SLSQP', bounds=((0, 1), (-100, 100), (0, 10000)))  # , jac=Lprime)
+        self.T = est.x[0]
+        self.c = est.x[1]
+        self.R = est.x[2]
+        print('omega', est.x[1])
+        print('phi', est.x[0])
+        print('sigma_eta', np.sqrt(est.x[2]))
+
+    def iterate(self, plot=True, estimate=False, init_params=None):
+        """Iterate over the observations and update the filtered values after each iteration"""
+        # Create empty arrays to store values
+        F = np.zeros(len(self.y))
+        a = np.zeros(len(self.y))
+        v = np.zeros(len(self.y))
+        P = np.zeros(len(self.y))
+        # Initialize at the initial values parsed to the class
+        if estimate == True:
+            self.T = np.array(init_params[0])
+            self.c = np.array(init_params[1])
+            self.R = np.array(init_params[2])
+        P[0] = self.P_start
+        a[0] = self.a_start
+        # Iterate
+        for t in range(0, len(self.y) - 1):
+            v[t] = self.y[t] - self.Z * a[t] - self.d
+            F[t] = self.Z * P[t] * self.Z.transpose() + self.H
+            a_t = a[t] + ((P[t] * self.Z.transpose()) / F[t]) * v[t]
+            a[t + 1] = self.T * a_t + self.c
+            P_t = P[t] - ((P[t] * self.Z.transpose()) / F[t]) * self.Z * P[t]
+            P[t + 1] = self.T * P_t * self.T.transpose() + self.R * self.Q * self.R.transpose()
+        F[-1] = P[-1] + self.H
+        v[-1] = self.y[-1] - a[-1]
+
+        # Obtain std error of prediction form variance
+        std = np.sqrt((P * self.H) / (P + self.H))
+
+        if plot:
+            plot_fig2_1(self.times, self.y, a, std, P, v, F)
+        return a, std, P, v, F
+
+    def state_smooth(self, plot=True):
+        a, std, P, v, F = self.iterate(plot=False)
+        # Obtain all time values for L
+        L = self.T - P / F
+
+        # Do the recursion for r
+        r = np.zeros(len(self.y))
+        N = np.zeros(len(self.y))
+        V = np.zeros(len(self.y))
+
+        for t in np.arange(len(self.y) - 1, -1, -1):
+            r[t-1] = (self.Z.transpose()/F[t])*v[t] + L[t] * r[t]
+        for t in np.arange(len(self.y) - 1, 0, -1):
+            N[t-1] = (self.Z.transpose()/F[t])*self.Z + L[t]**2 * N[t]
+        for t in np.arange(len(self.y) - 1, 0, -1):
+            V[t] = P[t] - P[t] ** 2 * N[t - 1]
+        V[0] = V[-1]
+        N[0] = N[-2]
+
+        # Do the recursion for alpha
+        alphas = np.zeros(len(self.y))
+        alphas[0] = a[t]
+        for t in range(0, len(self.y)):
+            alphas[t] = a[t] + P[t] * r[t - 1]
+        alphas[-1] = np.nan
+        std = np.sqrt(V)[1:]
+        if plot:
+            plot_fig2_2(self.times, self.y, alphas, std, V, r, N)
+        return alphas, N
--- Assignment2/main.py
+++ Assignment2/main.py
@@ -6,10 +6,12 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import kurtosis, skew
 
 # Import our own libs with functions
 from plottinglib import *
-from kalman import *
+from kalman import KFclass
+from kalman_new import KFnew
 from kalman_prediction import *
 
 def DK_book():
@@ -20,11 +22,28 @@ def DK_book():
     df = pd.read_table('sv.dat')
     df.columns = ['returns'] # dollar exchange rate
     df['returns'] = df['returns'] / 100
-    print_stats(df['returns'])
+    
     x_t = np.log((df['returns']-np.mean(df['returns']))**2)
     df['transformed_returns'] = x_t
+    print('The sample moments of X_t are: ', 'mean = ', np.round(np.mean(df['transformed_returns']),2))
+    print('variance = ', np.round(np.var(df['transformed_returns']),2))
+    print('skewness = ', np.round(skew(df['transformed_returns']), 2))
+    print('kurtosis = ', np.round(kurtosis(df['transformed_returns']), 2))
+    # df.x = df.x-df.x.mean()
+    # df['y'] = np.log(1+df['x']/100)
+    
 
-    plot_raw_data(df)
+    # plt.figure(figsize=(8,3))
+    # plt.plot(df.index, df.y, lw=0.3)
+    # plt.ylim(-0.035, 0.05)
+    # plt.show()
+
+    #    df['transformed_returns'] = np.log(df['y']**2)
+    
+    #-np.mean(df.returns)
+    #print(df.transformed_returns)
+    # plot raw data
+    #plot_raw_data(df)
 
     # Perform QML-method
     #QMLplot(df)
@@ -38,15 +57,12 @@ def DK_book():
     KFobj.pardict = {'phi': 0.99,
                      'omega': -0.08,
                      'sigma_eta2':0.083**2}
+
     KFobj.fit_model()
-    KFobj.state_smooth(plot=True)
-    #print(KFobj.iterate(plot=False)[0])
-    #plt.plot(KFobj.state_smooth(plot=False)[0],color='black',lw=1)
-    #plt.scatter(df.index, df['transformed_returns'], s=1)
-    #plt.show()
-    #plt.plot(100*np.pi/np.sqrt(2)*np.exp(0.5*KFobj.state_smooth(plot=False)[0]))
-    #plt.plot(1e2*np.exp(0.5*KFobj.state_smooth(plot=False)[0]),color='black',lw=1)
-    #plt.show()
+    # print(KFobj.iterate(plot=False)[0])
+    plt.plot(KFobj.state_smooth(plot=False)[0],color='black',lw=1)
+    plt.scatter(df.index, df['transformed_returns'], s=1)
+    plt.show()
     #print(alphas)
     
 """
@@ -61,9 +77,106 @@ KFobj.disturbance_smoothing()
 KFobj.missing_data()
 """
 
+def SP500():
+    # Set matplotlib style for fancy plotting
+    plt.style.use('MNRAS_stylesheet')
+
+    # Load the data
+    df = pd.read_csv('oxfordmanrealizedvolatilityindices.csv', index_col=0)
+    df = df[df['Symbol'] == '.SPX']
+    df['returns'] = (df['close_price'] - df['close_price'].shift(1))/100
+    df['logreturns'] = np.log(df['close_price']) - np.log(df['close_price'].shift(1))
+    df['transformed_returns'] = np.log((df['logreturns'] - np.mean(df['logreturns'])) ** 2)
+    df.index = np.arange(0, len(df), 1)
+
+    init_parameters = {'phi': 0.99,
+                       'omega': -0.08,
+                       'sigma_eta2':0.083**2}
+    KFobj = KFclass(df, init_parameters,'transformed_returns', var_name='Transformed returns')
+    KFobj.init_state_matrices()
+    print(-KFobj.__llik_fun__([0.1, -0.0878, 0.0837**2]))
+
+    KFobj.pardict = {'phi': 0.1,
+                     'omega': -0.08,
+                     'sigma_eta2':0.083**2}
+
+    # KFobj.fit_model()
+    # print(KFobj.iterate(plot=False)[0])
+    plt.plot(KFobj.state_smooth(plot=False)[0],color='black',lw=1)
+    plt.scatter(df.index, df['transformed_returns'], s=1)
+    plt.show()
+
+
+def nile_data():
+    # Import required libs
+
+    # Set matplotlib style for fancy plotting
+    plt.style.use('MNRAS_stylesheet')
+
+    # Load the data
+    df = pd.read_table('Nile.dat')
+    df.columns = ['volume']
+    df['year'] = np.arange(1871, 1971, 1)
+    df = df.set_index('year')
+    df = pd.DataFrame(df)
+    df = df.rename(columns={'volume': 'dep_var'})
+
+    # Define the system matrices
+    Z = np.array(1)
+    R = np.array(1)
+    T = np.array(1)
+    d = 0
+    c = 0
+    H = np.array(15099)
+    Q = np.array(1469.1)
+
+    # Run Kalman filter and reproduce fig 2.1
+    # Create Kalman filter object
+    KFobj = KFnew(df, Z=Z, R=R, d=d, c=c, H=H, Q=Q, T=T, var='dep_var')
+    KFobj.init_filter(a=df['dep_var'].iloc[0], P=1e7)
+    KFobj.state_smooth(plot=True)
+
+def DK_book_new():
+    # Set matplotlib style for fancy plotting
+    plt.style.use('MNRAS_stylesheet')
+
+    # Load the data
+    df = pd.read_table('sv.dat')
+    df.columns = ['returns']  # dollar exchange rate
+    df['returns'] = df['returns'] / 100
+
+    x_t = np.log((df['returns'] - np.mean(df['returns'])) ** 2)
+    df['transformed_returns'] = x_t
+    print('The sample moments of X_t are: ', 'mean = ', np.round(np.mean(df['transformed_returns']), 2))
+    print('variance = ', np.round(np.var(df['transformed_returns']), 2))
+    print('skewness = ', np.round(skew(df['transformed_returns']), 2))
+    print('kurtosis = ', np.round(kurtosis(df['transformed_returns']), 2))
+    # Define the system matrices
+    Z = np.array(1)
+    R = np.array(np.sqrt(0.083**2))  # to be estimated
+    T = np.array(0.99)  # to be estimated
+    d = np.array(-1.27)
+    c = np.array(-0.08) # to be estimated
+    H = np.array(np.sqrt(np.pi**2/2))
+    Q = np.array(1)
+
+    # init_parameters = {'T': 0.99,
+    #                    'omega': -0.08,
+    #                    'sigma_eta2':0.083**2}
+    # Run Kalman filter and reproduce fig 2.1
+    # Create Kalman filter object
+    KFobj = KFnew(df, Z=Z, R=R, d=d, c=c, H=H, Q=Q, T=T, var='transformed_returns')
+    KFobj.init_filter(a=-10, P=R / (1-T**2))
+    # KFobj.fit_model()
+    # KFobj.state_smooth(plot=True)
+    plt.plot(KFobj.state_smooth(plot=False)[0],color='black',lw=1)
+    plt.scatter(df.index, df['transformed_returns'], s=1)
+    plt.show()
 
 def main():
-    DK_book()
+    DK_book_new()
+    # SP500()
+    # nile_data()
 
 if __name__ == "__main__":
     main()
