fixed loglikelihood for first part

Assignment2/code_Mees.py
@@ -79,9 +79,14 @@ def kalmanFilter(x_t, theta):
     for t in range(1, N):
         v_t[t] = x_t.iloc[t - 1] - a_t[t] - c  # fixed mean adjustment
         F_t[t] = P_t[t] + sig_eps
+
+
         k_t[t] = (phi * P_t[t]) / F_t[t]
         a_t[t + 1] = omega + phi * a_t[t] + k_t[t] * v_t[t]  # omega is the mean adjustment to be estimated
+        print(v_t[t], F_t[t], P_t[t], a_t[t])
+        # print(a_t[t])
         P_t[t + 1] = phi ** 2 * P_t[t] + sig_eta - (k_t[t] ** 2) * F_t[t]
+        # print(phi ** 2 * P_t[1] + sig_eta - (k_t[1] ** 2) * F_t[1], sig_eta)
     return [a_t, P_t, v_t, F_t]
 
 def estimation(x_t):
@@ -94,7 +99,7 @@ def estimation(x_t):
                        sig_ini
                       ])
     options ={'eps':1e-09,  # argument convergence criteria
-              'disp': True,  # display iterations
+              'disp': False,  # display iterations
               'maxiter':200} # maximum number of iterations
     
     results = scipy.optimize.minimize(likelihood, theta_ini, args=(x_t),
@@ -139,7 +144,6 @@ def likelihood(theta, x_t):
     l = (N - 1)/2 - 0.5 * np.sum(np.log(np.abs(F_t[2:]))) - 0.5 * np.sum((v_t[2:]**2) / F_t[2:])
 
     llik = -np.mean(l)
-
     return llik
 
 def basicDescriptives(data):
@@ -195,12 +199,12 @@ def main():
     r_t = data      #y_t: Daily logreturns of pound-dollar exchange rates, mean corrected
     y_t = r_t/100                       #tranformation by dividing by 100
     #plot_a(y_t)
-    
-    print("Mean:",basicDescriptives(y_t)[0])
-    print("Median:",basicDescriptives(y_t)[1])
-    print("Std:",basicDescriptives(y_t)[2])
-    print("Skewness:",basicDescriptives(y_t)[3])
-    print("Kurtosis:",basicDescriptives(y_t)[4])
+    #
+    # print("Mean:",basicDescriptives(y_t)[0])
+    # print("Median:",basicDescriptives(y_t)[1])
+    # print("Std:",basicDescriptives(y_t)[2])
+    # print("Skewness:",basicDescriptives(y_t)[3])
+    # print("Kurtosis:",basicDescriptives(y_t)[4])
     
     #for i in range(5):
     #    print(basicDescriptives(y_t)[i]) #mean,median,std,skewness,kurtosis
@@ -211,14 +215,13 @@ def main():
     x_t = np.log((y_t - np.mean(y_t))**2)
     # plot_b(x_t)
     
-    
     ###Question c###
-    omega = estimation(x_t)[0]
-    print("omega:",omega)
-    phi = estimation(x_t)[1]
-    print("phi:",phi)
-    sig_eta = estimation(x_t)[2]
-    print("sig_eta:",sig_eta)
+    # omega = estimation(x_t)[0]
+    # print("omega:",omega)
+    # phi = estimation(x_t)[1]
+    # print("phi:",phi)
+    # sig_eta = estimation(x_t)[2]
+    # print("sig_eta:",sig_eta)
 
     omega = -0.015  # estimated value for intercept from book
     phi = 0.995  # estimated value for ar coefficient from book
@@ -232,28 +235,27 @@ def main():
     
     #define vectors for AR(1)+noise model
     a_t = kalmanFilter(x_t,theta)[0]  # Filtered state a_t
-    P_t = kalmanFilter(x_t,theta)[1]  # Filtered state variance p_t,
-    v_t = kalmanFilter(x_t,theta)[2]  # Prediction errors v_t
-    F_t = kalmanFilter(x_t,theta)[3]  # Prediction variance F_t
-
-    alpha_t = kalmanFilter_smooth(a_t, P_t, v_t, F_t, x_t, theta)[0]
-    V_t = kalmanFilter_smooth(a_t, P_t, v_t, F_t, x_t, theta)[1]
-    r_t = kalmanFilter_smooth(a_t, P_t, v_t, F_t, x_t, theta)[2]
-    N_t = kalmanFilter_smooth(a_t, P_t, v_t, F_t, x_t, theta)[3]
-    
-    #simulated y's and alpha's
-    y_plus = simulation(N, alpha_t, V_t, theta)[0]
-    y_plus = pd.DataFrame(y_plus)
-
-    alpha_plus = simulation(N, alpha_t, V_t, theta)[1]
-
-    a_t_sim = kalmanFilter(y_plus, theta)[0]  # Filtered state a_t
-    P_t_sim = kalmanFilter(y_plus, theta)[1]  # Filtered state variance p_t,
-    v_t_sim = kalmanFilter(y_plus, theta)[2]  # Prediction errors v_t
-    F_t_sim = kalmanFilter(y_plus, theta)[3]  # Prediction variance F_t
-    alpha_plus_hat = kalmanFilter_smooth(a_t_sim, P_t_sim, v_t_sim, F_t_sim, y_plus, theta)[0]
-
-    h_t = alpha_t[2:] + alpha_plus[1:] - alpha_plus_hat[2:]
+    # P_t = kalmanFilter(x_t,theta)[1]  # Filtered state variance p_t,
+    # v_t = kalmanFilter(x_t,theta)[2]  # Prediction errors v_t
+    # F_t = kalmanFilter(x_t,theta)[3]  # Prediction variance F_t
+    # alpha_t = kalmanFilter_smooth(a_t, P_t, v_t, F_t, x_t, theta)[0]
+    # V_t = kalmanFilter_smooth(a_t, P_t, v_t, F_t, x_t, theta)[1]
+    # r_t = kalmanFilter_smooth(a_t, P_t, v_t, F_t, x_t, theta)[2]
+    # N_t = kalmanFilter_smooth(a_t, P_t, v_t, F_t, x_t, theta)[3]
+    #
+    # #simulated y's and alpha's
+    # y_plus = simulation(N, alpha_t, V_t, theta)[0]
+    # y_plus = pd.DataFrame(y_plus)
+    #
+    # alpha_plus = simulation(N, alpha_t, V_t, theta)[1]
+    #
+    # a_t_sim = kalmanFilter(y_plus, theta)[0]  # Filtered state a_t
+    # P_t_sim = kalmanFilter(y_plus, theta)[1]  # Filtered state variance p_t,
+    # v_t_sim = kalmanFilter(y_plus, theta)[2]  # Prediction errors v_t
+    # F_t_sim = kalmanFilter(y_plus, theta)[3]  # Prediction variance F_t
+    # alpha_plus_hat = kalmanFilter_smooth(a_t_sim, P_t_sim, v_t_sim, F_t_sim, y_plus, theta)[0]
+    #
+    # h_t = alpha_t[2:] + alpha_plus[1:] - alpha_plus_hat[2:]
     
     # plot_d(x_t, h_t[2:])
     return

Assignment2/kalman_new.py
@@ -23,12 +23,13 @@ class KFnew():
         self.y = np.array(df[var].values.flatten())
         self.times = df.index
         self.options = {'eps': 1e-09,
-                        'maxiter': 2000}
+                        'maxiter': 200}
 
-    def init_filter(self, a, P, b=None):
+    def init_filter(self, a, P, b=None, alpha_mean=None):
         self.a_start = a
         self.b_start = b
         self.P_start = P
+        self.alpha_mean = alpha_mean
 
     def __llik_fun__(self, par_ini):
         # likelihood function of state space model
@@ -37,8 +38,7 @@ class KFnew():
             _, __, P, v, F = self.iterate(plot=False, estimate=True, init_params=par_ini)
         elif self.method == 'iterateRegression':
                 _, __, P, v, F = self.iterateRegression(plot=False, estimate=True, init_params=par_ini)
-        L= (n / 2)*np.log(2*np.pi) - 0.5 * np.sum(np.log(np.abs(F[1:]))) - 0.5 * np.sum((v[1:] ** 2) / F[1:]) #+ np.log(self.P_start)
-        # print(L)
+        L= -(n / 2)*np.log(2*np.pi) - 0.5 * np.sum(np.log(np.abs(F[1:]))) - 0.5 * np.sum((v[1:] ** 2) / F[1:]) #+ np.log(self.P_start)
         return (-1)*L
 
     def fit_model(self, phi, omega, sigma_eta, beta=0):
@@ -48,7 +48,7 @@ class KFnew():
         Lprime = lambda x: approx_fprime(x, self.__llik_fun__, 0.01)
         est = minimize(self.__llik_fun__, x0=par_ini,
                        options=self.options,
-                       method='BFGS', bounds=((-1, 1), (-100, 100), (0, 10000), (-1, 1)), jac=Lprime)
+                       method='BFGS') #, jac=Lprime)
         return est.x
 
     def iterate(self, plot=True, estimate=False, init_params=None):
@@ -65,19 +65,21 @@ class KFnew():
             self.R = np.array(init_params[2])
             # self.H = np.array(init_params[0])
             # self.Q = np.array(init_params[1])
-        P[0] = self.P_start
+        P[0] = self.P_start #0.8035 / (1 - 0.995 ** 2)
         a[0] = self.a_start
         # Iterate
         for t in range(0, len(self.y) - 1):
             v[t] = self.y[t] - self.Z * a[t] - self.d
             F[t] = self.Z * P[t] * self.Z.transpose() + self.H
-            # print(v[t], F[t], P[t])
             a_t = a[t] + ((P[t] * self.Z.transpose()) / F[t]) * v[t]
             a[t + 1] = self.T * a_t + self.c
+            # print(v[t], F[t], P[t], a[t], self.y[t])
             P_t = P[t] - ((P[t] * self.Z.transpose()) / F[t]) * self.Z * P[t]
             P[t + 1] = self.T * P_t * self.T.transpose() + self.R * self.Q * self.R.transpose()
+            # print(self.T * P_t * self.T.transpose() + self.R * self.Q * self.R.transpose(), self.R * self.Q * self.R.transpose() )
         F[-1] = P[-1] + self.H
         v[-1] = self.y[-1] - a[-1]
+
         # Obtain std error of prediction form variance
         std = np.sqrt((P * self.H) / (P + self.H))
         if plot:
@@ -96,7 +98,7 @@ class KFnew():
             self.T = np.array([[init_params[0],0],[0,1]])
             self.c = np.vstack(([init_params[1],0]))
             self.R = np.vstack(([init_params[2]],[0]))
-            self.a_start = np.vstack(([-10.89], [init_params[3]]))
+            self.a_start = np.vstack(([self.alpha_mean], [init_params[3]]))
             # self.H = np.array(init_params[0])
             # self.Q = np.array(init_params[1])
         P[0,:,:] = self.P_start
@@ -150,3 +152,9 @@ class KFnew():
         if plot:
             plot_fig2_2(self.times, self.y, alphas, std, V, r, N)
         return alphas, N
+
+    def particle_filter(self, phi, sigma_eta2, N):
+        theta, _ = self.state_smooth()
+        for i in range(len(theta)):
+            n_draws = np.random.normal(loc=phi*theta[i], scale=sigma_eta2, size=N)
+            # weigths = np.exp(-0.5*np.log())

Assignment2/main.py
@@ -88,34 +88,38 @@ def DK_book_new():
 
     x_t = np.log((df['returns'] - np.mean(df['returns'])) ** 2)
     df['transformed_returns'] = x_t
+    print(df['transformed_returns'])
     print('The sample moments of X_t are: ', 'mean = ', np.round(np.mean(df['transformed_returns']), 2))
     print('variance = ', np.round(np.var(df['transformed_returns']), 2))
     print('skewness = ', np.round(skew(df['transformed_returns']), 2))
     print('kurtosis = ', np.round(kurtosis(df['transformed_returns']), 2))
     # Define the system matrices
+    sigma_eta = 0.0991
+    phi = 0.99
+    omega = -0.08
     Z = np.array(1)
-    R = np.array(0.0837)  # to be estimated
-    T = np.array(0.9912)  # to be estimated
+    R = np.array(sigma_eta)  # to be estimated
+    T = np.array(phi)  # to be estimated
     d = np.array(-1.27)
-    c = np.array(-0.0878) # to be estimated
+    c = np.array(omega) # to be estimated
     H = np.array(np.pi**2/2)
     Q = np.array(1)
 
     # Run Kalman filter and reproduce fig 2.1
     # Create Kalman filter object
     KFobj = KFnew(df, Z=Z, R=R, d=d, c=c, H=H, Q=Q, T=T, var='transformed_returns')
-    KFobj.init_filter(a=-0.0878/(1-0.9912), P=0.0837**2) # R / (1 - T ** 2))
-    print(KFobj.__llik_fun__(par_ini=[0.9912, -0.0878, 0.0837]))
-    # estimates = KFobj.fit_model(phi=np.array(0.99), omega=np.array(-0.0878), sigma_eta=np.array(0.0837) )
-    # # print(estimates)
-    # KFobj.T = np.array(estimates[0])
-    # KFobj.c = np.array(estimates[1])
-    # KFobj.R = np.array(estimates[2])
+    KFobj.init_filter(a=(omega/(1-phi)), P= sigma_eta**2 / (1 - phi ** 2))
+    estimates = KFobj.fit_model(phi=np.array(phi), omega=np.array(omega), sigma_eta=np.array(sigma_eta) )
+    print(KFobj.__llik_fun__(par_ini=[estimates[0], estimates[1], estimates[2]]))
+    print(estimates)
+    KFobj.T = np.array(estimates[0])
+    KFobj.c = np.array(estimates[1])
+    KFobj.R = np.array(estimates[2])
     # #
     # # # KFobj.state_smooth(plot=True)
-    # plt.plot(KFobj.state_smooth(plot=False)[0][1:],color='black',lw=1)
-    # plt.scatter(df.index, df['transformed_returns'], s=1)
-    # plt.show()
+    plt.plot(KFobj.state_smooth(plot=False)[0][1:],color='black',lw=1)
+    plt.scatter(df.index, df['transformed_returns'], s=1)
+    plt.show()
 
 
 
@@ -131,42 +135,52 @@ def SP500():
     df = df.iloc[1:]
     df['transformed_returns'] = np.log((df['logreturns'] - np.mean(df['logreturns'])) ** 2)
     df.index = np.arange(0, len(df), 1)
-    # Define the system matrices
-    Z = np.array(1)
-    R = np.array(np.sqrt(0.083**2))  # to be estimated
-    T = np.array(0.99)  # to be estimated
-    d = np.array(-1.27)
-    c = np.array(-0.08) # to be estimated
-    H = np.array(np.pi**2/2)
-    Q = np.array(1)
 
-    # Create Kalman filter object
-    KFobj = KFnew(df, Z=Z, R=R, d=d, c=c, H=H, Q=Q, T=T, var='transformed_returns', method='iterate')
-    KFobj.init_filter(a=-10.89, P=R/(1-T**2))
-    estimates = KFobj.fit_model(phi=np.array(0.99), omega=np.array(-0.08), sigma_eta=np.array(np.sqrt(0.083**2)) )
-    print(estimates)
-    KFobj.T = np.array(estimates[0])
-    KFobj.c = np.array(estimates[1])
-    KFobj.R = np.array(estimates[2])
-    # KFobj.iterate()
-    plt.plot(KFobj.state_smooth(plot=False)[0],color='black',lw=1)
-    plt.scatter(df.index, df['transformed_returns'], s=1)
-    plt.show()
+    sigma_eta = 0.0991
+    phi = 0.99
+    omega = -0.08
+    alpha_mean = omega / (1 - phi)
+    alpha_var = sigma_eta ** 2 / (1 - phi ** 2)
+    # Define the system matrices
+    # Z = np.array(1)
+    # R = np.array(np.sqrt(0.83**2))  # to be estimated
+    # T = np.array(0.99)  # to be estimated
+    # d = np.array(-1.27)
+    # c = np.array(-0.08) # to be estimated
+    # H = np.array(np.pi**2/2)
+    # Q = np.array(1)
+    #
+    # # Create Kalman filter object
+    # KFobj = KFnew(df, Z=Z, R=R, d=d, c=c, H=H, Q=Q, T=T, var='transformed_returns', method='iterate')
+    # KFobj.init_filter(a=-10.89, P=R/(1-T**2))
+    # estimates = KFobj.fit_model(phi=np.array(0.99), omega=np.array(-0.08), sigma_eta=np.array(np.sqrt(0.083**2)) )
+    # print(estimates)
+    # KFobj.T = np.array(estimates[0])
+    # KFobj.c = np.array(estimates[1])
+    # KFobj.R = np.array(estimates[2])
+    # # KFobj.iterate()
+    # plt.plot(KFobj.state_smooth(plot=False)[0],color='black',lw=1)
+    # plt.scatter(df.index, df['transformed_returns'], s=1)
+    # plt.show()
 
     Z = np.vstack((np.ones(len(df)), np.log(df['rk_parzen'].values)))
-    R = np.vstack(([np.sqrt(0.083**2)], [0]))
-    T = np.array([[0.99, 0], [0, 1]])
+    R = np.vstack(([sigma_eta], [0]))
+    T = np.array([[phi, 0], [0, 1]])
     d = np.array([-1.27])
-    c = np.vstack(([-0.08], [0]))
+    c = np.vstack(([omega], [0]))
     H = np.array([np.pi**2/2])
     Q = np.array([1])
     KFobj = KFnew(df, Z=Z, R=R, d=d, c=c, H=H, Q=Q, T=T, var='transformed_returns', method='iterateRegression')
-    a_b_start = np.vstack(([-10.89], [0.1]))
-    P_start = np.array([[0.083/(1-0.99**2), 0], [0, 0]])
-    KFobj.init_filter(a=a_b_start, P=P_start)
+
+    # initialize beta
+    beta = 0.05
+    a_b_start = np.vstack(([alpha_mean], [beta]))
+    P_start = np.array([[alpha_var, 0], [0, 0]])
+    KFobj.init_filter(a=a_b_start, P=P_start, b=beta, alpha_mean=alpha_mean)
 
     # fit the model
-    # estimates = KFobj.fit_model(phi=0.8, omega=-0.08, sigma_eta=np.sqrt(0.083**2), beta=0.15)
+    # estimates = KFobj.fit_model(phi=phi, omega=omega, sigma_eta=sigma_eta, beta=beta)
+    # print(estimates)
     # phi = estimates[0]
     # omega = estimates[1]
     # sigma_eta = estimates[2]
@@ -174,7 +188,8 @@ def SP500():
     # KFobj.T = np.array([[phi, 0], [0, 1]])
     # KFobj.c = np.vstack(([omega], [0]))
     # KFobj.R = np.vstack(([sigma_eta], [0]))
-    # KFobj.a_start = np.vstack(([-10.89], [beta]))
+    # KFobj.a_start = np.vstack(([omega / (1 - phi)], [beta]))
+    # KFobj.P_start = np.array([[sigma_eta ** 2 / (1 - phi ** 2), 0], [0, 0]])
 
     # run iteration
     a, std, P, v, F= KFobj.iterateRegression(plot=False)
@@ -184,8 +199,8 @@ def SP500():
     plt.show()
 
 def main():
-    DK_book_new()
-    # SP500()
+    # DK_book_new()
+    SP500()
 
 if __name__ == "__main__":
     main()