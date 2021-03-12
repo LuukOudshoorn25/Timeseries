cd9883781ad0a0335ca3b4863809ec68df1504d0
LuukOudshoorn25 <luuk@lupanas.nl>
2021-03-12 22:29:20 +0100

    Added comments

--- Assignment2/kalman_new.py
+++ Assignment2/kalman_new.py
@@ -10,6 +10,7 @@ class KFnew():
         """Initialisation, where df is a pandas DataFrame and var is the name of the column to study and
            init_pars is a dictionary with initial values"""
         self.method = method
+        # Initialize all the system matrices as attributes of the class
         self.Z = Z
         self.Q = Q
         self.H = H
@@ -17,36 +18,45 @@ class KFnew():
         self.R = R
         self.d = d
         self.c = c
-
+        # same for parameters and objects
         self.df = df
         self.var = var
+        # The data itself
         self.y = np.array(df[var].values.flatten())
         self.times = df.index
+        # Options for the minimizer
         self.options = {'eps': 1e-09,
                         'maxiter': 200}
 
     def init_filter(self, a, P, b=None, alpha_mean=None):
+        """Initialize the filter"""
         self.a_start = a
         self.b_start = b
         self.P_start = P
         self.alpha_mean = alpha_mean
 
     def __llik_fun__(self, par_ini):
-        # likelihood function of state space model
+        """likelihood function of state space model,
+           to be optimized by minimizer. Pass parameter vector to it and obtain likelihood"""
         n = len(self.y)
+        # Only the normal KFS
         if self.method == 'iterate':
             _, __, P, v, F = self.iterate(plot=False, estimate=True, init_params=par_ini)
+        # KSF with regression coefficient (beta)
         elif self.method == 'iterateRegression':
                 _, __, P, v, F = self.iterateRegression(plot=False, estimate=True, init_params=par_ini)
         L= -(n / 2)*np.log(2*np.pi) - 0.5 * np.sum(np.log(np.abs(F[1:]))) - 0.5 * np.sum((v[1:] ** 2) / F[1:]) #+ np.log(self.P_start)
+        # Return negative likelihood since we minimize it with the minimizier
         return (-1)*L
 
     def fit_model(self, phi, omega, sigma_eta, beta=0):
+        """Optimize model by maximizing the likelihood"""
         # Initialize at the initial values parsed to the class
         par_ini = [phi, omega, sigma_eta, beta]
-        # minimize the likelihood function
+        # Approximate the jabocian for more efficient minimization
         Lprime = lambda x: approx_fprime(x, self.__llik_fun__, 0.01)
         if self.method == 'iterateRegression':
+            # Depending on whether we include the regression coefficient, use other optimizer
             est = minimize(self.__llik_fun__, x0=par_ini,
                            options=self.options,
                            method='Newton-CG', jac=Lprime)
@@ -54,6 +64,7 @@ class KFnew():
             est = minimize(self.__llik_fun__, x0=par_ini,
                            options=self.options,
                            method='BFGS')
+        # Return optimal parameters
         return est.x
 
     def iterate(self, plot=True, estimate=False, init_params=None):
@@ -70,24 +81,24 @@ class KFnew():
             self.R = np.array(init_params[2])
             # self.H = np.array(init_params[0])
             # self.Q = np.array(init_params[1])
-        P[0] = self.P_start #0.8035 / (1 - 0.995 ** 2)
+        P[0] = self.P_start
         a[0] = self.a_start
-        # Iterate
+        # Iterate over the observatios
         for t in range(0, len(self.y) - 1):
+            # Kalman filter equations
             v[t] = self.y[t] - self.Z * a[t] - self.d
             F[t] = self.Z * P[t] * self.Z.transpose() + self.H
             a_t = a[t] + ((P[t] * self.Z.transpose()) / F[t]) * v[t]
             a[t + 1] = self.T * a_t + self.c
-            # print(v[t], F[t], P[t], a[t], self.y[t])
             P_t = P[t] - ((P[t] * self.Z.transpose()) / F[t]) * self.Z * P[t]
             P[t + 1] = self.T * P_t * self.T.transpose() + self.R * self.Q * self.R.transpose()
-            # print(self.T * P_t * self.T.transpose() + self.R * self.Q * self.R.transpose(), self.R * self.Q * self.R.transpose() )
         F[-1] = P[-1] + self.H
         v[-1] = self.y[-1] - a[-1]
 
         # Obtain std error of prediction form variance
         std = np.sqrt((P * self.H) / (P + self.H))
         if plot:
+            # Plot the data
             plot_fig2_1(self.times, self.y, a, std, P, v, F)
         return a, std, P, v, F
 
@@ -106,9 +117,9 @@ class KFnew():
             self.a_start = np.vstack(([self.alpha_mean], [init_params[3]]))
         P[0,:,:] = self.P_start
         a_b[:,0:1] = self.a_start
-        # print(np.shape(a_b[:,0:1]), P[0,:,:], a_b[:,0:1])
         # Iterate
         for t in range(0, len(self.y) - 1):
+            # Slightly different updating equations for KF since we now have regression coefficient
             v[t] = self.y[t] - np.dot(self.Z[:,t:t+1].T,a_b[:,t]) - self.d
             F[t] = np.dot(np.dot(self.Z[:,t:t+1].T, P[t]),self.Z[:,t:t+1]) + self.H
             a_t = a_b[:,t:t+1] + np.dot(P[t],self.Z[:,t:t+1] / F[t]) * v[t]
@@ -129,17 +140,21 @@ class KFnew():
         r = np.zeros((2, len(self.y)))
         N = np.zeros((len(self.y), 2, 2))
         V = np.zeros((len(self.y), 2, 2))
-
+        # Instead of normal KF equations, we now iterate backward in time
         for t in np.arange(len(self.y) - 1, -1, -1):
+            # Obtain K, L and r
             K = np.dot(self.T, np.dot(P[t], self.Z[:,t:t+1] / F[t]))
             L = self.T - np.dot(K, self.Z[:,t:t+1].T)
             r[:,t-1:t] = (self.Z[:,t:t+1]/F[t])*v[t] + np.dot(L.T, r[:,t:t+1])
         for t in np.arange(len(self.y) - 1, 0, -1):
+            # Obtain N
             K = np.dot(self.T, np.dot(P[t], self.Z[:,t:t+1] / F[t]))
             L = self.T - np.dot(K, self.Z[:,t:t+1].T)
             N[t-1] = np.dot(self.Z[:,t:t+1]/F[t],self.Z[:,t:t+1].T) + np.dot(L**2, N[t])
         for t in np.arange(len(self.y) - 1, 0, -1):
+            # Obtain V
             V[t] = P[t] - np.dot(P[t] ** 2, N[t - 1])
+        # Recursion to avoid infinites etc
         V[0] = V[-1]
         N[0] = N[-2]
 
@@ -160,7 +175,7 @@ class KFnew():
         r = np.zeros(len(self.y))
         N = np.zeros(len(self.y))
         V = np.zeros(len(self.y))
-
+        # Iterate backward in time for r, N, V
         for t in np.arange(len(self.y) - 1, -1, -1):
             r[t-1] = (self.Z.transpose()/F[t])*v[t] + L[t] * r[t]
         for t in np.arange(len(self.y) - 1, 0, -1):
@@ -181,10 +196,13 @@ class KFnew():
         return alphas, N
 
     def particle_filter(self, estimates,y):
+        # Obtain smoothed state
         alphas, _ = self.state_smooth(plot=False)
+        # Obtain parameter estimates from likelihood maximization
         phi, omega, sigma_eta,_ = estimates
-        print(y)
+        # Define weight function
         weights = lambda y,theta: np.exp(-0.5*np.log(2*np.pi) - 0.5*np.exp(theta+xi)-0.5*y**2 / np.exp(theta+xi))
+        # Make output array for resampled mean, variance and effective sampel size
         outputs = np.zeros((3,len(alphas)-2))        
         for i in range(0,len(alphas[:-1])):
             # step 1: draw N values alpha_tilde N(at-1,sigma_eta)
@@ -193,15 +211,23 @@ class KFnew():
                 # Initial values
                 samples = np.random.randn(10000)*(sigma_eta/(1-phi**2))
             else:
+                # get signal by subtracting xi
                 theta = alphas[i-1] - xi
+                # random sample around phi*theta
                 samples = np.random.randn(10000)*sigma_eta + phi*theta
             # step 2: compute corresponding weights
             w = weights(y[i], samples)
+            # Normalize weights such that sum = 1
             w = w / np.sum(w)
+            # Get effective sample size
             ESS = (np.sum(w**2)**(-1))
+            # Get signal by meaning over samples
             signal_hat = np.sum(w*samples)
+            # Get variance by taking mean of signal squared minus expectation value squared
             P_hat = np.sum(w*samples**2)-signal_hat**2
+            # Resample using stratified sampling
             resampling = np.mean(np.random.choice(samples, p=w, size=10000))
+            # Save outputs
             outputs[0,i-1] = resampling
             outputs[1,i-1] = P_hat
             outputs[2,i-1] = ESS
--- Assignment2/main.py
+++ Assignment2/main.py
@@ -12,24 +12,28 @@ from scipy.stats import kurtosis, skew
 from plottinglib import *
 from kalman import KFclass
 from kalman_new import KFnew
-from kalman_prediction import *
 
 def DK_book_new():
+    """Function to run all Kalman Related stuff for the dollar-pound exchange rate"""
     # Set matplotlib style for fancy plotting
     plt.style.use('MNRAS_stylesheet')
 
     # Load the data
     df = pd.read_table('sv.dat')
     df.columns = ['returns']  # dollar exchange rate
+    # Divide the returns to get true returns instead of percentges
     df['returns'] = df['returns'] / 100
-
+    # Convert the returns and demean
     x_t = np.log((df['returns'] - np.mean(df['returns'])) ** 2)
+    # Save in the dataframe
     df['transformed_returns'] = x_t
 
     # Define the system matrices
+    # Initial guesses
     sigma_eta = 0.0991
     phi = 0.99
     omega = -0.08
+    # Plug these in the system matrices
     Z = np.array(1)
     R = np.array(sigma_eta)  # to be estimated
     T = np.array(phi)  # to be estimated
@@ -42,21 +46,23 @@ def DK_book_new():
     # Create Kalman filter object
     KFobj = KFnew(df, Z=Z, R=R, d=d, c=c, H=H, Q=Q, T=T, var='transformed_returns')
     KFobj.init_filter(a=(omega/(1-phi)), P= sigma_eta**2 / (1 - phi ** 2))
+    # Run the optimizer itself
     estimates = KFobj.fit_model(phi=np.array(phi), omega=np.array(omega), sigma_eta=np.array(sigma_eta) )
-    
+    # Extract the estimated parameters and update the system matrices
     KFobj.T = np.array(estimates[0])
     KFobj.c = np.array(estimates[1])
     KFobj.R = np.array(estimates[2])
-    #
+
     xi = KFobj.c/(1-KFobj.T)
+    # Get from the Kalman Filter object the KF and KFS signals
     filtered_signal = KFobj.iterate(plot=False)[0][1:]
     smoothed_signal = KFobj.state_smooth(plot=False)[0][1:]
+    # Plot the smoothed signal
     #plot_smoothed(df=df, filtered_alphas=filtered_signal, smoothed_alphas=smoothed_signal, xi=xi)
     # Particle filter
-    print(estimates)
     PF_outputs = KFobj.particle_filter(estimates,df['returns'] - np.mean(df['returns']))
     #plot_pf(PF_outputs)
-    plot_Hts(PF_outputs[0,:],smoothed_signal,filtered_signal,estimates)
+    #plot_Hts(PF_outputs[0,:],smoothed_signal,filtered_signal,estimates)
 
 def SP500_regression():
     # Set matplotlib style for fancy plotting
@@ -72,13 +78,13 @@ def SP500_regression():
 
     # plot_raw_data(df)
     # set parameters
-    sigma_eta = 0.20175882
-    phi =  0.98399089
-    omega = -0.06589895
+    sigma_eta = 0.202
+    phi =  0.984
+    omega = -0.066
     beta = -0.1
     alpha_mean = omega / (1 - phi)
     alpha_var = sigma_eta ** 2 / (1 - phi ** 2)
-
+    # Again initialize the system matrices but now add the realized kernel estiamtes to it
     Z = np.vstack((np.ones(len(df)), np.log(df['rk_parzen'].values)))
     R = np.vstack(([sigma_eta], [0]))
     T = np.array([[phi, 0], [0, 1]])
@@ -87,18 +93,20 @@ def SP500_regression():
     H = np.array([np.pi ** 2 / 2])
     Q = np.array([1])
     KFobj = KFnew(df, Z=Z, R=R, d=d, c=c, H=H, Q=Q, T=T, var='transformed_returns', method='iterateRegression')
-
+    # We have instead of only alpha, alpha and beta in a vector
     a_b_start = np.vstack(([alpha_mean], [beta]))
+    # Same holds for P
     P_start = np.array([[alpha_var, 0], [0, 0]])
     KFobj.init_filter(a=a_b_start, P=P_start, b=beta, alpha_mean=alpha_mean)
 
-    # # fit the model
+    # Fit the model and estimate all four coefficients
     estimates = KFobj.fit_model(phi=phi, omega=omega, sigma_eta=sigma_eta, beta=beta)
     print(estimates)
     phi = estimates[0]
     omega = estimates[1]
     sigma_eta = estimates[2]
     beta = estimates[3]
+    # Update the system matrices
     KFobj.T = np.array([[phi, 0], [0, 1]])
     KFobj.c = np.vstack(([omega], [0]))
     KFobj.R = np.vstack(([sigma_eta], [0]))
@@ -112,6 +120,9 @@ def SP500_regression():
     plot_smoothed(df=df, filtered_alphas=filtered_signal, smoothed_alphas=smoothed_signal, xi=xi, fname='KFregression_SP500.pdf')
 
 def SP500():
+    """Identical to DK_book_new but for SP500 data
+       see code comments in that function for explanation etc
+    """
     # Set matplotlib style for fancy plotting
     plt.style.use('MNRAS_stylesheet')
 
@@ -125,9 +136,9 @@ def SP500():
     df.index = np.linspace(2000, 2021, len(df))
 
     # plot_raw_data(df)
-    sigma_eta = 0.20175882
-    phi =  0.98399089
-    omega = -0.06589895
+    sigma_eta = 0.202
+    phi =  0.984
+    omega = -0.0659
     alpha_mean = omega / (1 - phi)
     alpha_var = sigma_eta ** 2 / (1 - phi ** 2)
     # Define the system matrices
@@ -138,7 +149,7 @@ def SP500():
     c = np.array(omega) # to be estimated
     H = np.array(np.pi**2/2)
     Q = np.array(1)
-    #
+    
     # Create Kalman filter object
     KFobj = KFnew(df, Z=Z, R=R, d=d, c=c, H=H, Q=Q, T=T, var='transformed_returns', method='iterate')
     KFobj.init_filter(a=alpha_mean, P=alpha_var)
@@ -165,6 +176,9 @@ def SP500():
 
 
 def main():
+    """Main function, from here we call the functions for SP500. 
+       SP500 with regression (extension 1) and the dollar-pound echange rate data
+    """
     # DK_book_new()
     SP500()
     # SP500_regression()
